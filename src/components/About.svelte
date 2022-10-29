f270f8530c8b0de5d71bdb5427ff9fb8570c3699
Manojna <manojnakp2003@gmail.com>
2022-10-29 12:55:33 +0530

    modularise
    split about into two column layout reusable for other components

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,53 +1,24 @@
 <script>
     import src from '../assets/gears.png';
+    import TwoCol from "./TwoCol.svelte";
 </script>
 
-<div id="about" class="about-container">
-    <div class="about">
-        <h2>About</h2>
-        <h3>Project Vision</h3>
-        <div class="two-column">
-            <img {src} alt="pattern design" />
-            <div class="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-                Praesent libero. Sed cursus ante dapibus diam.
-                Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                Duis sagittis ipsum. Praesent mauris.
-                Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                Duis sagittis ipsum. Praesent mauris.
-            </div>
-        </div>
+<TwoCol id="about" heading="About" subHeading="Project Vision">
+    <img {src} alt="pattern design" slot="left" />
+    <div class="text" slot="right">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam.
+        Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+        Duis sagittis ipsum. Praesent mauris.
+        Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+        Duis sagittis ipsum. Praesent mauris.
     </div>
-</div>
-
+</TwoCol>
 <style>
-    .about {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        max-width: 80vw;
-        margin: 0 auto;
-    }
-    h2 {
-        color: var(--primary);
-    }
-    h2, h3 {
-        text-align: center;
-    }
-    .two-column {
-        display: flex;
-        gap: 2rem;
-    }
     img, .text {
         width: 15rem;
     }
     @media (max-width: 720px) {
-        .two-column {
-            flex-direction: column;
-            align-items: center;
-            width: 100%;
-            padding: 1rem;
-        }
         .text {
             width: 100%;
         }

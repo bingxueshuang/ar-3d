ad89a7db68d00f6934e52bdf49ecbb5531b0b488
Manojna <manojnakp2003@gmail.com>
2022-10-29 14:18:39 +0530

    card skeleton
    design a reusable card

---
+++ src/components/Card.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let heading, subHeading;
+</script>
+
+<div class="card">
+    <div class="image">
+        <slot name="image" />
+    </div>
+    <div class="content">
+        <h3>{heading}</h3>
+        {#if subHeading}
+            <h4>{subHeading}</h4>
+        {/if}
+        <div class="text">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</div>
--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,16 +1,25 @@
 <script>
     import TwoCol from "./TwoCol.svelte";
+    import Card from "./Card.svelte";
 </script>
 
 <TwoCol id="services" heading="Services" subHeading="What the project offers">
-    <div class="text" slot="right">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-        Praesent libero. Sed cursus ante dapibus diam.
-        Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-        Duis sagittis ipsum. Praesent mauris.
-        Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-        Duis sagittis ipsum. Praesent mauris.
-    </div>
+    <Card heading="One" slot="left">
+        <div slot="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+            Praesent libero. Sed cursus ante dapibus diam.
+            Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+            Duis sagittis ipsum. Praesent mauris.
+        </div>
+    </Card>
+    <Card heading="Two" slot="right">
+        <div slot="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+            Praesent libero. Sed cursus ante dapibus diam.
+            Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+            Duis sagittis ipsum. Praesent mauris.
+        </div>
+    </Card>
 </TwoCol>
 <style>
     img, .text {
